2e10e65218f42c3cb0e0688ec365c46c2260f4fa
Noah Burrell <noah@burrell.tech>
2024-01-03 14:53:44 -0500

    update

--- demos/workflows-infrastructure/configs/events/test.sh
+++ demos/workflows-infrastructure/configs/events/test.sh
@@ -2,8 +2,39 @@
 
 set -xe
 
-readarray nodePools < <( gcloud container clusters describe upgrade-demo --zone us-central1-a --project akuity-training | yq e -o=j -I=0 '.nodePools[]' )
+CLUSTER_VERSION=$(echo "1.26.5-gke.2100")
+CLUSTER_NAME=$(echo "upgrade-demo")
+CLUSTER_PROJECT=$(echo "akuity-training")
+CLUSTER_ZONE=$(echo "us-central1-a")
+
+CLUSTER_DESCRIPTION=$(gcloud container clusters describe ${CLUSTER_NAME} --zone ${CLUSTER_ZONE} --project ${CLUSTER_PROJECT})
+
+CUR_MASTER_VERS=$(echo "${CLUSTER_DESCRIPTION}" | yq e '.currentMasterVersion')
+CUR_NODE_VERS=$(echo "${CLUSTER_DESCRIPTION}" | yq e '.currentNodeVersion')
+
+if [ "${CUR_MASTER_VERS}" == "${CLUSTER_VERSION}" ]; then
+    echo "Current master version matches!"
+else
+    echo "Current master version does not match!"
+    exit 1
+fi
+
+if [ "${CUR_MASTER_VERS}" == "${CLUSTER_VERSION}" ]; then
+    echo "Current node version matches!"
+else
+    echo "Current node version does not match!"
+    exit 1
+fi
+
+readarray nodePools < <( echo "${CLUSTER_DESCRIPTION}" | yq e -o=j -I=0 '.nodePools[]' )
 for nodePool in "${nodePools[@]}"; do
     nodePoolName=$(echo "$nodePool" | yq e '.name' -)
-    echo "NodePool: $nodePoolName"
+    nodePoolVersion=$(echo "$nodePool" | yq e '.version' -)
+
+    if [ "${nodePoolVersion}" == "${CLUSTER_VERSION}" ]; then
+        echo "${nodePoolName} nodepool version matches!"
+    else
+        echo "${nodePoolName} nodepool version does not match!"
+        exit 1
+    fi
 done
